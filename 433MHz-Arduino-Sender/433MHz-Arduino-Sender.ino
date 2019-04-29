b6c703b4812b3cd413bea740a4cbb382900065db
techdiem <48885218+techdiem@users.noreply.github.com>
2019-04-29 16:27:58 +0200

    initial commit

---
+++ 433MHz-Arduino-Sender/433MHz-Arduino-Sender.ino
@@ -0,0 +1,40 @@
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  mySwitch.enableTransmit(10); //Pin D10 on my chinese Nano
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  //send data only when it receives data:
+  if (Serial.available() > 0) {
+
+    String cmdvalue  = Serial.readStringUntil(';');
+    Serial.read(); //next character is semicolon, skip it
+    String systemcode = Serial.readStringUntil(';');
+    Serial.read();
+    String unitcode  = Serial.readStringUntil('\0');
+
+    if (cmdvalue == "ON") {
+      mySwitch.switchOn(string2char(systemcode), string2char(unitcode));
+      Serial.println("Switching on " + systemcode + " " + unitcode);
+    } else if (cmdvalue == "OFF") {
+      mySwitch.switchOff(string2char(systemcode), string2char(unitcode));
+      Serial.println("Switching off " + systemcode + " " + unitcode);
+    } else if (cmdvalue == "Test") {
+      Serial.println("I am up and running!");
+    } else {
+      Serial.println("Wrong command! Use ON or OFF.");
+    }
+  }
+}
+
+char* string2char(String command) {
+  if (command.length() != 0) {
+    char *p = const_cast<char*>(command.c_str());
+    return p;
+  }
+}
---
+++ rfsend.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import serial
+from time import sleep
+import sys
+
+#Get data from command line
+cmd = sys.argv[1]
+systemcode = sys.argv[2]
+unitcode = sys.argv[3]
+
+if not (cmd == "ON" or cmd == "OFF" or systemcode or unitcode):
+    print("Usage: rfsend.py <ON/OFF> <Systemcode> <Unitcode>")
+else:
+    #Connect to Arduino: Change serial port here!
+    try:
+        ser = serial.Serial("/dev/ttyUSB0",9600, timeout=1)
+        ser.flushInput()
+    except:
+        print("Connection to Arduino failed, check Serialport!")
+
+    sleep(2)
+    #Send data
+    ser.write(bytes(cmd + ";" + systemcode + ";" + unitcode, 'utf-8'))
+    line = ser.readall()
+    print("Result: ", line)
+    sleep(1)
+
+    #Disconnect
+    ser.close()
